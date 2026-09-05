d9efc16e06d635c7958afac0e009436de48e059c
hehehe521 <129355827+hehehe521@users.noreply.github.com>
2026-09-05 20:29:51 +0800

    Update metadata title in layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,7 @@ import { TooltipProvider } from '@/components/ui/tooltip';
 import packageJson from '../package.json';
 
 export const metadata = {
-  title: `基估宝 V${packageJson.version}`,
+  title: `呵呵估值`,
   description: '输入基金编号添加基金，实时显示估值与前10重仓'
 };
 
